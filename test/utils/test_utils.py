938bf4ecc983b51f995e00545d8c85f31da24098
cesarealice <cesarematteor@gmail.com>
2025-02-24 17:26:25 -0500

    compare profiling function

--- test/utils/test_utils.py
+++ test/utils/test_utils.py
@@ -1,12 +1,27 @@
 """
 
 """
-from utils.utils import generate_profiling_report
+from utils.utils import generate_profiling_report, compare_profiling_report
+
+
 def test_generate_profiling_report():
     title= "Report Profiling"
-    report_filepath = "traffic_preprocessed.html"
-    data_filepath = "/home/cesarealice/PycharmProjects/generative-ml-malicious-traffic-wolf-in-sheeps-clothes/test/pipeline/preprocess/traffic_preprocessed.csv"
+    report_name = 'traffic_preprocessed'
+    report_filepath = f"{report_name}.html"
+    data_filepath = f"/home/cesarealice/PycharmProjects/generative-ml-malicious-traffic-wolf-in-sheeps-clothes/test/pipeline/preprocess/{report_name}.csv"
     type_schema = {'Label': "categorical"}
 
     generate_profiling_report(report_filepath=report_filepath, title=title, data_filepath=data_filepath,
                               type_schema=type_schema, minimal=False)
+
+
+def test_compare_profiling_reports():
+    title= "Report Profiling"
+    report_name = 'compare_profiling_reports'
+    report_filepath = f"{report_name}.html"
+    data_filepath_a = f"/home/cesarealice/PycharmProjects/generative-ml-malicious-traffic-wolf-in-sheeps-clothes/test/pipeline/preprocess/traffic_preprocessed.csv"
+    data_filepath_b = f"/home/cesarealice/PycharmProjects/generative-ml-malicious-traffic-wolf-in-sheeps-clothes/test/pipeline/preprocess/traffic_preprocessed_train.csv"
+    type_schema = {'Label': "categorical"}
+
+    compare_profiling_report(report_filepath=report_filepath, title=title, data_filepath_a=data_filepath_a,
+                             data_filepath_b=data_filepath_b, type_schema=type_schema, minimal=False)
--- utils/utils.py
+++ utils/utils.py
@@ -18,3 +18,19 @@ def generate_profiling_report(title:str, report_filepath:str,  df: pd.DataFrame=
     df_profile = ProfileReport(df, title=title, minimal=minimal, type_schema=type_schema)
     # export profiling report
     df_profile.to_file(report_filepath)
+
+
+def compare_profiling_report(title:str, report_filepath:str,  data_filepath_a:str, data_filepath_b:str,
+                              type_schema=None, minimal:bool=True):
+    # read dfs
+    a_df = pd.read_csv(data_filepath_a)
+    b_df = pd.read_csv(data_filepath_b)
+
+    # generate data profiling report
+    a_df_profile = ProfileReport(a_df, title=title, minimal=minimal, type_schema=type_schema)
+    b_df_profile = ProfileReport(b_df, title=title, minimal=minimal, type_schema=type_schema)
+
+    # compare reports
+    comparison_report = a_df_profile.compare(b_df_profile)
+    # export profiling report
+    comparison_report.to_file(report_filepath)
